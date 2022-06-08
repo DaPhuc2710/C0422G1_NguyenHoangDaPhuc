89637ceabfb331294e02790269851c3e8e2acc6c
DaPhuc2710 <daphuc2000@gmail.com>
2022-06-09 05:14:54 +0700

    111

--- module2/src/ss2_loop_statement/bai_tap/Geometry.java
+++ module2/src/ss2_loop_statement/bai_tap/Geometry.java
@@ -1,7 +1,6 @@
 package ss2_loop_statement.bai_tap;
 
 
-
 import java.util.Scanner;
 
 public class Geometry {
@@ -9,7 +8,7 @@ public class Geometry {
         char c = '*';
         int choice = -1, i, j;
         Scanner inp = new Scanner(System.in);
-        while (choice != 0){
+        while (choice != 0) {
             System.out.println("---------MENU---------");
             System.out.println("1 | Print the rectangle.");
             System.out.println("2 | Print the square triangle.");
@@ -17,36 +16,40 @@ public class Geometry {
             System.out.println("4 | Exit.");
             System.out.println("Enter Number:");
             choice = inp.nextInt();
-            switch (choice){
+            switch (choice) {
                 case 1:
-                    for (i = 1; i <= 5; i++){
-                        for (j = 1; j <= 7; j++){
+                    for (i = 1; i <= 5; i++) {
+                        for (j = 1; j <= 7; j++) {
                             System.out.print(c + "  ");
                         }
                         System.out.print("\n");
                     }
                     break;
                 case 2:
-                    for (i = 1; i <= 5; i++){
-                        for (j = 1; j <= 5; j++){
+                    for (i = 1; i <= 5; i++) {
+                        for (j = 1; j <= 5; j++) {
                             System.out.print(c + "  ");
                         }
                         System.out.print("\n");
                     }
-                    break;
-                case 3:
-                    for (i = 1; i <= 5; i++){
-                        for (j = 1; j <= i; j++){
+                    for (i = 1; i <= 5; i++) {
+                        for (j = 1; j <= i; j++) {
                             System.out.print(c + "  ");
                         }
                         System.out.print("\n");
                     }
                     System.out.println("\n");
-                    for (int a = 1; a <= 5; a++){
-                        for (int b = 5; b >= a; b--){
-                            System.out.print(c + "  ");
+                    break;
+                case 3:
+                    for (int m = 0; m < 5; m++) {
+                        for (int k = 5 - m; k > 0; k--) {
+                            System.out.print("   ");
                         }
-                        System.out.print("\n");
+                        for (int n = 0; n < 2 * m - 1; n++) {
+                            System.out.print(" * ");
+
+                        }
+                        System.out.println();
                     }
                     break;
                 case 0:
--- module2/src/tester/ss2/TesterSs2.java
+++ module2/src/tester/ss2/TesterSs2.java
@@ -3,8 +3,8 @@ package tester.ss2;
 public class TesterSs2 {
     public static void main(String[] args) {
        char c='*';
-        for (int i = 0; i < ; i++) {
-            
+        for (int i = 0; i < 1; i++) {
+            System.out.println(c);
         }
     }
 }
