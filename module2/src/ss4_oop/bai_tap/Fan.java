b0eb7e47aadd491ac3d1ad33ed952d9731b86d45
DaPhuc2710 <daphuc2000@gmail.com>
2022-06-10 23:11:37 +0700

    1111

--- module2/src/ss4_oop/bai_tap/Fan.java
+++ module2/src/ss4_oop/bai_tap/Fan.java
@@ -1,5 +1,7 @@
 package ss4_oop.bai_tap;
 
+import jdk.nashorn.internal.parser.Scanner;
+
 public class Fan {
     final int SLOW = 1; // final là khai báo hằng số
     final int MEDIUM = 2;
@@ -63,7 +65,18 @@ public class Fan {
         }
     }
     public static void main(String[] args) {
-
+        Fan fan1 = new Fan ();
+        fan1.setSpeed(3);
+        fan1.setRadius(10);
+        fan1.setColor("yellow");
+        fan1.setOn(true);
+        Fan fan2 = new Fan ();
+        fan2.setSpeed(2);
+        fan2.setRadius(5);
+        fan2.setColor("blue");
+        fan2.setOn(false);
+        System.out.println(fan1.toString());
+        System.out.println(fan2.toString());
     }
 }
 
