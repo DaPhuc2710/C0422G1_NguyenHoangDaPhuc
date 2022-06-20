ff2afc147d9a237e6beca43b0f9e45b6341683f2
DaPhuc2710 <daphuc2000@gmail.com>
2022-06-20 22:16:55 +0700

    1111

--- module2/src/ss10_stack_queue/bai_tap/bai_tap_4/Bstack.java
+++ module2/src/ss10_stack_queue/bai_tap/bai_tap_4/Bstack.java
@@ -1,5 +1,35 @@
 package ss10_stack_queue.bai_tap.bai_tap_4;
 
-public class Bstack {
+import java.util.Scanner;
+import java.util.Stack;
 
+public class Bstack {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap bieu thuc");
+        String string = scanner.nextLine();
+        Stack<Character> stack = new Stack<>();
+        boolean flag = true;
+        for (int i = 0; i < string.length(); i++) {// (()))
+            if (string.charAt(i) == '(') {
+                stack.push(string.charAt(i));
+            } else if (string.charAt(i) == ')') {
+                if (stack.isEmpty()) {
+                    flag = false;
+                    break;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        if (!flag) {
+            System.out.println("false");
+        } else if (stack.isEmpty()) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
 }
+
+
